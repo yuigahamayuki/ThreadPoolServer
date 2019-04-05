588feccea44d23ad18cbc0c56c1b388218dca698
NinooniN <yuigahamarem@163.com>
2019-04-05 14:00:07 +0800

    added c++11 compile option

--- processSimple.cpp
+++ processSimple.cpp
@@ -4,9 +4,9 @@
 void ProcessSimple::process(int connfd)
 {
 	const char *sendBuf = "Welcome!";
-	/*for (int i = 0; i < 50000; ++i)
+	for (int i = 0; i < 50000; ++i)
 	{
-	}*/
+	}
 	send(connfd, sendBuf, sizeof(sendBuf), 0);
 	//close(connfd);
 }
